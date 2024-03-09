64b3dd35605921015909b44ac69e9605873c8ee3
Zachary Sidnam <zac@sidnam.com>
2024-03-09 11:42:08 -0800

    more tooling changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+import { inter } from '@/app/fonts';
+
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
